6f040c318c3813f5b3cefe51df1dd16d2ec8c4a8
Traumtanzen <gvardser@gmail.com>
2019-09-13 02:16:10 +0300

    Really final

--- Sphynx/Program.cs
+++ Sphynx/Program.cs
@@ -7,8 +7,8 @@ namespace Sphynx
         static void Main(string[] args)
         {
         Start:
-            Console.WriteLine("Hello Stranger! You're now at the beginning of a dangerous journey." +
-            "Are you ready to start?");
+            Console.WriteLine("Hello Stranger! You're now at the beginning of a dangerous journey.");
+            Console.WriteLine("Are you ready to start?");
             bool notYes = true;
             while (notYes)
             {
@@ -16,9 +16,9 @@ namespace Sphynx
                 switch (firstAnswer.ToLower())
                 {
                     case "yes":
-                        Console.WriteLine("A large door in the rock is opened before you and you did a step inside.");
+                        Console.WriteLine("A large door in the rock is opened before you and you made a step inside.");
                         Console.WriteLine("The darkness surrounds and you see only a small plate on the wall in front of you.");
-                        Console.WriteLine("It says \"Next you'll have no right for a mistake\"");
+                        Console.WriteLine("It says: \"Next you'll have no right for a mistake\".");
                         Console.WriteLine("On reading these lines you feel that something stares at you. What will you do?");
                         notYes = false;
                         break;
@@ -27,19 +27,19 @@ namespace Sphynx
                         notYes = true;
                         goto case "yes";
                     default:
-                        Console.WriteLine("You must choose <yes> or <no>.");
+                        Console.WriteLine("You must type <yes> or <no>.");
                         break;
                 }
             }
             Console.WriteLine("You may <run>, <turn back> or <do nothing>");
-            bool notTurn = false;
+            bool notTurn = true;
             while (notTurn)
             {
                 string secondAnswer = Console.ReadLine();
                 switch (secondAnswer.ToLower())
                 {
                     case "run":
-                        Console.WriteLine("You started to run from the horrifying stare and suddenly fell to the abyss.");
+                        Console.WriteLine("You started to run from the horrifying stare and suddenly fell into the abyss.");
                         notTurn = true;
                         goto BadEnd;
                     case "do nothing":
@@ -53,56 +53,59 @@ namespace Sphynx
                 }
             }
         MeetSphynx:
-            Console.WriteLine("The time for riddles has come.");
+            Console.WriteLine("He looked at you with his sparkling eyes and said \"The time for riddles has come.");
             Console.WriteLine("So, the first one is:");
-            Console.WriteLine("I am born of water but when I return to water, I die. What am I?");
-            bool notIce = false;
+        FirstQuestion:
+            Console.WriteLine("\"I am born of water but when I return to water, I die. What am I?\"");
+            bool notIce = true;
             while (notIce)
             {
                 string firstRiddle = Console.ReadLine();
                 switch (firstRiddle.ToLower())
                 {
                     case "ice":
-                        Console.WriteLine("Well done! The next riddle.");
+                        Console.WriteLine("\"Well done! The next riddle.");
                         notIce = true;
-                        break;
+                        goto SecondQuestion;
                     default:
-                        Console.WriteLine("You should think harder. My patience's for you. Try again.");
+                        Console.WriteLine("\"You should think harder. My patience's for you. Listen again:");
                         notIce = false;
-                        break;
+                        goto FirstQuestion;
                 }
             }
-            Console.WriteLine("What gives you the strength and power to walk through a wall?");
-            bool notDoor = false;
+        SecondQuestion:
+            Console.WriteLine("\"What gives you the strength and power to walk through a wall?\"");
+            bool notDoor = true;
             while (notDoor)
             {
                 string secondRiddle = Console.ReadLine();
                 switch (secondRiddle.ToLower())
                 {
                     case "door":
-                        Console.WriteLine("Well. We're coming to an end.");
+                        Console.WriteLine("\"Well. We're coming to an end.");
                         notDoor = true;
-                        break;
+                        goto LastQuestion;
                     default:
-                        Console.WriteLine("Think harder! My patience is almost out.");
+                        Console.WriteLine("\"Think harder! My patience is almost out. I repeat:");
                         notDoor = false;
-                        break;
+                        goto SecondQuestion;
                 }
             }
+        LastQuestion:
             Console.WriteLine("Now you'll have only one chance. Listen attentively and show your best.");
-            Console.WriteLine("Which creature walks on four legs in the morning, two legs in the afternoon, and three legs in the evening?");
-            bool notMan = false;
+            Console.WriteLine("\"Which creature walks on four legs in the morning, two legs in the afternoon, and three legs in the evening?\"");
+            bool notMan = true;
             while (notMan)
             {
                 string lastRiddle = Console.ReadLine();
                 switch (lastRiddle.ToLower())
                 {
                     case "man":
-                        Console.WriteLine("Great! For now you'll be rewarded.");
+                        Console.WriteLine("\"Great! For now you'll be rewarded.\"");
                         notMan = true;
-                        break;
+                        goto GoodEnd;
                     default:
-                        Console.WriteLine("You should think harder. My patience's for you. Try again.");
+                        Console.WriteLine("\"You should think harder. My patience's for you. Try again.\"");
                         notMan = false;
                         goto PreBadEnd;
                 }
@@ -117,7 +120,7 @@ namespace Sphynx
         BadEnd:
             Console.WriteLine("Your journey is over. You died.");
         Ending:
-            Console.WriteLine("Press <1> to start once again or press any key to quit.");
+            Console.WriteLine("Enter <1> to start once again or any key to quit.");
             string answer = Console.ReadLine();
             if (answer == "1")
             {
@@ -127,6 +130,7 @@ namespace Sphynx
             {
                 Console.WriteLine("Bye.");
             }
+            Console.ReadLine();
         }
 
     }
